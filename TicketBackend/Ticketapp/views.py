6be2a70b9df6363f5a553770e6b06b5af6fcf2fc
manvisingh3097 <singh.thakurmanvi@gmail.com>
2024-01-14 18:08:18 +0530

    working on ticket app

--- TicketBackend/Ticketapp/views.py
+++ TicketBackend/Ticketapp/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.views import views
+from django.views import APIView
 import json
+from .serializers import *
+from rest_framework.response import Response
 
 # Create your views here.
 
 class UserView(APIView):
-    def get(self, request):
+    def POST(self, request):
+        serializer = UserSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Account created succesfully"} , status=201)
+        return Response(serializer.errors , status=400)
+
+
